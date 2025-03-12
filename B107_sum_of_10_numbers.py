5152d6564ef33ea782fdc14ddbcf780a0b537605
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:02:57 +0800

    Prints sum of 10 numbers

--- B107_sum_of_10_numbers.py
+++ B107_sum_of_10_numbers.py
@@ -1,3 +1,10 @@
 # Batch 1 Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+sum = 0
+
 # Input 10 numbers (for loop)
+for num in range(0, 10):
+    number = float(input(f"Enter number {num + 1}: "))
+    sum += number
+
 # Print sum
+print(f"The sum of the ten numbers is {sum}")
