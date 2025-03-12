9cb702a075f487a6a46bcc534b9bb6d1623bad7f
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:26:26 +0800

    Count odd numbers in list

--- B108_odd_numbers_out_of_10.py
+++ B108_odd_numbers_out_of_10.py
@@ -9,4 +9,7 @@ for num in range (0, 10):
     number = float(input(f"Enter number {num + 1}: "))
 
 # Append and count odd numbers
+    if number % 2 != 0:
+        odd_count += 1
+        odd_numbers.append(number)
 # Print list and count
