3187a9a3499e78ba1d0e304d0e953d62019c885a
Eunice <reyes05eunice@gmail.com>
2025-03-12 18:54:26 +0800

    First commit, uncorrected append

--- B401_duplicates_out_of_10_numbers.py
+++ B401_duplicates_out_of_10_numbers.py
@@ -8,8 +8,10 @@ duplicates = []
 for num in range(0, 10):
     number = float(input(f"Enter number {num + 1}: "))
     num_list.append(number)
-    
+
 # Append duplicates
+for number in num_list:
+    if num_list.count(number) != 1:
+        duplicates.append(number)
 
-
-# Print list
+print(duplicates)
