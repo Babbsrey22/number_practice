f698b199b67fae200b9912343a4fab9593e7e377
Eunice <reyes05eunice@gmail.com>
2025-03-12 17:34:06 +0800

    Input number and print lowest num

--- B304_print_lowest_number.py
+++ B304_print_lowest_number.py
@@ -1,5 +1,17 @@
 # Prog04: Create a program that ask user to input a number,
 # Continue asking until the user input is invalid;
 # Display the lowest number
+
 # Input inifinte amount of numbers until ValueError
-# From inputs, print lowest number
+num_list = []
+
+while True:
+    try:
+        number = int(input("Enter any number: "))
+        num_list.append(number)
+    except ValueError:
+        print("Stopping number input...")
+        print(num_list)
+        lowest_num = min(num_list)
+        print(f"The lowest number is {lowest_num}")
+        break
