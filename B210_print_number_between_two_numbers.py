5f9b37bc54486489bb7d9706162befcd42956bd3
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:16:05 +0800

    Print numbers in between num1 and num2

--- B210_print_number_between_two_numbers.py
+++ B210_print_number_between_two_numbers.py
@@ -2,7 +2,13 @@
 # Print all the numbers between the two numbers
 
 # Input 2 numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 # Print after num1 to before num2 (excluding both numbers)
+in_between = []
+
+for num in range(num1 + 1, num2):
+    in_between.append(num)
+
+print(in_between)
