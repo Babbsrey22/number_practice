ddd4636b79fc8084f47eb481eb00aff88b5a6e6e
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:33:32 +0800

    Append even numbers

--- B109_even_numbers_0-100.py
+++ B109_even_numbers_0-100.py
@@ -1,4 +1,10 @@
 # Prog09: Create a program that print all the even numbers
 # Starting from 0 to 100 (Use for loop)
+
+even_numbers = []
+
 # Append even numbers 0-100
+for num in range(0, 10):
+    if num % 2 == 0:
+        even_number.append(num)
 # Print list
