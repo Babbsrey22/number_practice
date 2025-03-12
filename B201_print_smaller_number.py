e63e5dffbabefdd471ba6a6e0fbe071a61ebd576
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:51:57 +0800

    Prints smaller number

--- B201_print_smaller_number.py
+++ B201_print_smaller_number.py
@@ -1,4 +1,12 @@
 # Prog01: Create a program that ask user to input 2 numbers,
 # Print the smaller number
+
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print smaller number
+if num1 < num2:
+    print(f"{num1} is smaller than {num2}")
+else:
+    print(f"{num2} is smaller than {num1}")  
