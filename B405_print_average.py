4a32289b475d530ea7d70a1c41b84d4504bd2b03
Eunice <reyes05eunice@gmail.com>
2025-03-12 20:15:21 +0800

    Input numbers

--- B405_print_average.py
+++ B405_print_average.py
@@ -1,6 +1,14 @@
 # Prog05: Create a program that ask user to input a number, 
 # Continue asking until the user input is invalid;
 # Display the average
+
 # Input infinite amount of numbers until ValueError
+num_list = []
+
+while True:
+    try:
+        number = int(input("Enter any number: "))
+        num_list.append(number)
+    except ValueError:
 # From inputs, compute average
-# Print average
+# Print average 
