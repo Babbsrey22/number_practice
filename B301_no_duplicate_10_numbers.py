b868a9668db847e7e21df71db98128d54c24d087
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:52:53 +0800

    First commit, append numbers uncorrected

--- B301_no_duplicate_10_numbers.py
+++ B301_no_duplicate_10_numbers.py
@@ -1,9 +1,15 @@
 # Prog01: Create a program that ask user to input 10 numbers, 
 # Display all numbers that don't have duplicate 
 
+num_list = []
+
 # Input 10 numbers 
 for num in range(0, 10):
     number = int(input(f"Enter number {num + 1}: "))
-    
+    num_list.append(number)
+
 # Append numbers with no duplicate 
-# Print list 
+
+for num in num_list:
+    if number.count(num) == 1:
+        print(number)
