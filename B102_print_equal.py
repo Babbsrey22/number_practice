742256c4876c1b732fe562d6458f4b707c31f5de
Eunice <reyes05eunice@gmail.com>
2025-03-12 11:01:56 +0800

    Prints equal

--- B102_print_equal.py
+++ B102_print_equal.py
@@ -1,3 +1,9 @@
 # Batch 1 Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print "Equal" if they are equal
+if num1 == num2:
+    print("Equal")
+    pass
