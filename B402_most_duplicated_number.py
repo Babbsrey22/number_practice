b06c59583c63affcdd0a35d0580e1283a0245110
Eunice <reyes05eunice@gmail.com>
2025-03-12 19:33:30 +0800

    Input numbers

--- B402_most_duplicated_number.py
+++ B402_most_duplicated_number.py
@@ -1,6 +1,18 @@
 # Prog02: Create a program that ask user to input a number,
 # Continue asking until the user input is invalid;
 # Display the number with the most number of duplicate
+
 # Input infinite amount of number until ValueError
-# Append and count duplicates
-# Print number with highest duplicate count
+
+num_list = []
+most_common_number = 0
+
+while True:
+    try:
+        for num in range(0, 10):
+            number = int(input(f"Enter number {num + 1}: "))
+            num_list.append(number)
+    except ValueError:
+        break
+
+print(f"The numbers input are {num_list}")
