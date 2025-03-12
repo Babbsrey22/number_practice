0ebc247354c6fd7098c223160dc4e5b5a03dd6d3
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:27:43 +0800

    Initial commit

---
+++ B301_no_duplicate_10_numbers.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 10 numbers,
+# Display all numbers that don't have duplicate
+# Input 10 numbers
+# Append numbers with no duplicate
+# Print list
---
+++ B305_arrange_numbers_lowest_to_highest.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number,
+# Continue asking until the user input is invalid;
+# Display the number from lowest to highest,
+# Clue: sort() function
+# Input infinite amount of numbers until ValueError
+# From inputs, arrange low to high
+# Print list
