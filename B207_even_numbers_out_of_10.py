18a078a43790d2e9daaeb9a0af0121e0b3766364
Eunice <reyes05eunice@gmail.com>
2025-03-12 13:51:17 +0800

    Input 10 numbers

--- B207_even_numbers_out_of_10.py
+++ B207_even_numbers_out_of_10.py
@@ -1,5 +1,12 @@
 # Prog07: Create a program that ask user to input 10 numbers,
 # Print how many are even numbers
+
+even_count = 0
+even_number = []
+
 # Input 10 numbers (for loop)
+for num in range(0, 100):
+    number = float(input(f"Enter number {num + 1}: "))
+    
 # Append and count even numbers
 # Print list and count
