88a7bdc1a4feed89c7ee0328b81fd6c560bee917
Eunice <reyes05eunice@gmail.com>
2025-03-12 13:52:36 +0800

    Append even numbers

--- B207_even_numbers_out_of_10.py
+++ B207_even_numbers_out_of_10.py
@@ -7,6 +7,10 @@ even_number = []
 # Input 10 numbers (for loop)
 for num in range(0, 100):
     number = float(input(f"Enter number {num + 1}: "))
-    
+
 # Append and count even numbers
+    if number % 2 == 0:
+        even_count += 1
+        even_number.append(number)
+        
 # Print list and count
