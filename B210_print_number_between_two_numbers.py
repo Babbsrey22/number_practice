07e2eee36ec7aecc5f5377bdac6d6c73305d995d
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:13:08 +0800

    Input 2 numbers

--- B210_print_number_between_two_numbers.py
+++ B210_print_number_between_two_numbers.py
@@ -1,4 +1,8 @@
 # Prog10: Create a program that ask user to input 2 numbers,
 # Print all the numbers between the two numbers
+
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print after num1 to before num2 (excluding both numbers)
