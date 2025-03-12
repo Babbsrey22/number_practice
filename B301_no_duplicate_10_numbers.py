369b0d52110ede6bc162e3784ea774aef495c3af
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:40:15 +0800

    Input 10 numbers

--- B301_no_duplicate_10_numbers.py
+++ B301_no_duplicate_10_numbers.py
@@ -1,5 +1,9 @@
 # Prog01: Create a program that ask user to input 10 numbers, 
 # Display all numbers that don't have duplicate 
+
 # Input 10 numbers 
+for num in range(0, 10):
+    number = int(input(f"Enter number {num + 1}: "))
+    
 # Append numbers with no duplicate 
 # Print list 
