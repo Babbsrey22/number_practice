9b8254ec7a9bc871731ebc7dd210373cdf8ff27e
Eunice <reyes05eunice@gmail.com>
2025-03-12 13:45:02 +0800

    Input 10 numbers

--- B206_first_number_minus_rest.py
+++ B206_first_number_minus_rest.py
@@ -1,5 +1,12 @@
 # Prog06: Create a program that ask user to input 10 numbers,
 # Print the result of the first number minus all of the remaining numbers
+
+all_numbers = []
+
 # Input 10 numbers (for loop)
+for num in range(0, 10):
+    number = float(input(f"Enter number {num + 1}: "))
+    all_numbers.append(number)
+    
 # First number minus sum of last 9
 # Print result
