bd3a2c8be3c46d9347877606815ae2b45e8d8cd5
Eunice <reyes05eunice@gmail.com>
2025-03-12 18:28:26 +0800

    Input 10 numbers

--- B401_duplicates_out_of_10_numbers.py
+++ B401_duplicates_out_of_10_numbers.py
@@ -1,5 +1,15 @@
 # Prog01: Create a program that ask user to input 10 numbers,
 # Display all numbers that have duplicate
+
+num_list = []
+duplicates = []
+
 # Input 10 numbers
+for num in range(0, 10):
+    number = float(input(f"Enter number {num + 1}: "))
+    num_list.append(number)
+    
 # Append duplicates
+
+
 # Print list
