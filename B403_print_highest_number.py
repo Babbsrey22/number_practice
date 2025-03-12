1c49385fbddf487e1698ac690f8eb9f2a6c41fe9
Eunice <reyes05eunice@gmail.com>
2025-03-12 20:09:38 +0800

    Input numbers

--- B403_print_highest_number.py
+++ B403_print_highest_number.py
@@ -1,5 +1,12 @@
 # Prog03: Create a program that ask user to input a number, 
 # Continue asking until the user input is invalid;
 # Display the highest number
+
 # Input infinite amount of numbers until ValueError
+num_list = []
+
+while True:
+    try:
+        number = int(input("Enter any number: "))
+        num_list.append(number)
 # From inputs, print highest num
