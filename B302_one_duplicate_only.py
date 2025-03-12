d079b22aed80cec2a040e364ff57a3fb7a697f82
Eunice <reyes05eunice@gmail.com>
2025-03-12 15:14:33 +0800

    Input 10 numbers

--- B302_one_duplicate_only.py
+++ B302_one_duplicate_only.py
@@ -1,6 +1,15 @@
 # Prog02: Create a program that ask user to input 10 numbers,
 # Display all numbers,
 # For numbers with duplicate, display only the first entry
+
+num_list = []
+
 # Input 10 numbers
+for num in range(0, 10):
+    number = int(input(f"Enter number {num + 1}: "))
+    num_list.append(number)
+
+print(f"The numbers entered are {num_list}")
+
 # Append all numbers and those with one duplicate
 # Print list
