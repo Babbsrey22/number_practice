3f715da7eaf3423809edfaa003a84f34ca29f54b
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:40:17 +0800

    Append numbers not ending in -0

--- B110_no_multiples_of_10.py
+++ B110_no_multiples_of_10.py
@@ -1,4 +1,10 @@
 # Prog10: Create a program that print all the numbers,
 # Starting from 0 to 100 except numbers ending in zero
+
+not_multiple_10 = []
+
 # Append numbers 0-100 without multiples of 10
+for num in range(0, 101):
+    if num % 10 != 0:
+        not_10.append(num)
 # Print list
