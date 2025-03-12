d20a6a968f37eb13f90c0166d2ae093078367e83
Eunice <reyes05eunice@gmail.com>
2025-03-12 17:35:57 +0800

    Input number until ValueError

--- B305_arrange_numbers_lowest_to_highest.py
+++ B305_arrange_numbers_lowest_to_highest.py
@@ -2,6 +2,13 @@
 # Continue asking until the user input is invalid; 
 # Display the number from lowest to highest, 
 # Clue: sort() function 
+
 # Input infinite amount of numbers until ValueError 
+num_list = []
+
+while True:
+    try:
+        number = int(input("Enter any number: "))
+        num_list.append(number)
 # From inputs, arrange low to high 
 # Print list 
