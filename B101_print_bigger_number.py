402b0def6e9fd59125c3008533b0582a0b7689b5
Eunice <reyes05eunice@gmail.com>
2025-03-12 10:56:57 +0800

    Print bigger number

--- B101_print_bigger_number.py
+++ B101_print_bigger_number.py
@@ -1,3 +1,10 @@
 # Batch 1 Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print bigger number
+if num1 > num2:
+    print(f"{num1} is the bigger number.")
+else:
+    print(f"{num2} is the bigger number.")
