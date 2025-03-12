04709d199f9c26b9d6154c2664693e77910a0ebf
Eunice <reyes05eunice@gmail.com>
2025-03-12 11:10:54 +0800

    Prints sum

--- B103_sum_of_two_numbers.py
+++ B103_sum_of_two_numbers.py
@@ -1,3 +1,8 @@
 # Batch 1 Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print sum
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
