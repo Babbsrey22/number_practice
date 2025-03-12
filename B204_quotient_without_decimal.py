520cb3590c108070446e55e403b05c0e424aae94
Eunice <reyes05eunice@gmail.com>
2025-03-12 13:20:53 +0800

    Print quotient without decimal point

--- B204_quotient_without_decimal.py
+++ B204_quotient_without_decimal.py
@@ -6,3 +6,8 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 # Print quotient without decimal point
+if num1 > num2:
+    quotient = int(num1 / num2)
+else:
+    quotient = int(num2 / num1)
+print(f"The quotient of {num1} and {num2}, without the decimal point, is {quotient}.")
