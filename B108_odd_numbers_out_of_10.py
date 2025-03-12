c42f0ad890e2aba8c1a187168a27ac2424b1e215
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:23:54 +0800

    Input 10 numbers

--- B108_odd_numbers_out_of_10.py
+++ B108_odd_numbers_out_of_10.py
@@ -1,5 +1,12 @@
 # Prog08: Create a program that ask user to input 10 numbers,
-# Print how many are odd numbers.
+# Print how many are odd numbers
+
+odd_count = 0
+odd_numbers = []
+
 # Input 10 numbers (for loop)
+for num in range (0, 10):
+    number = float(input(f"Enter number {num + 1}: "))
+
 # Append and count odd numbers
 # Print list and count
