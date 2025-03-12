a042ac267ccd77529a40c03221234361156efc79
Eunice <reyes05eunice@gmail.com>
2025-03-12 14:10:30 +0800

    Append numbers not ending in -5 or -0

--- B209_no_multiples_of_5.py
+++ B209_no_multiples_of_5.py
@@ -1,4 +1,12 @@
 # Prog09: Create a program that print all the numbers,
 # Starting from 0 to 100 except numbers ending in zero or ending five
+
+num = 0
+not_multiple_of_5 = []
+
 # Append numbers 0-100 without multiples of 5 (while loop)
+while num < 100:
+    num += 1
+    if num % 5 != 0:
+        not_multiple_of_5.append(num)
 # Print list
