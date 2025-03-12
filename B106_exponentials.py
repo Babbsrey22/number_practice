4dca32589d15ab6f5ca4097b7db1fedee8790433
Eunice <reyes05eunice@gmail.com>
2025-03-12 11:45:40 +0800

    Prints exponential

--- B106_exponentials.py
+++ B106_exponentials.py
@@ -1,3 +1,8 @@
 # Batch 1 Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 # Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Print exponential
+exponential = num1 ** num2
+print(exponential)
