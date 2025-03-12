9cdcca65f60b342379d18cca4dd05aa46547b627
Eunice <reyes05eunice@gmail.com>
2025-03-12 12:14:04 +0800

    Pseudocode

---
+++ B108_odd_numbers_out_of_10.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that ask user to input 10 numbers,
+# Print how many are odd numbers.
+# Input 10 numbers (for loop)
+# Append and count odd numbers
+# Print list and count
